9b47f1cbb2330e146ca3aecd884a90238766477a
hasanshakru <hasanshakru@gmail.com>
2022-08-29 12:30:50 +0530

    display change

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,9 +3,9 @@ import { Grid, Paper, Typography } from "@mui/material";
 
 const VideoItem = ({video, onVideoSelect}) => {
   return (
-  <Grid item xs={8} container spacing={1}>
-    <Paper style={{ alignItems: 'center', cursor:'pointer'}} onClick= {() => onVideoSelect(video)} >
-        <img style={{marginRight: '10px'}} alt= "thumbnail" src={video.snippet.thumbnails.medium.url}/>
+  <Grid item xs={8} container spacing={1} columns={{xs:4, sm:8, md:12}}>
+    <Paper style={{display: 'flex', alignItems: 'center', cursor:'pointer'}} onClick= {() => onVideoSelect(video)} >
+        <img style={{marginRight: '20px'}} alt= "thumbnail" src={video.snippet.thumbnails.medium.url}/>
         <Typography variant="subtitle1"><b>{video.snippet.title}</b></Typography>
     </Paper>
   </Grid>
